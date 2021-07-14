3ddf5cc8d9b5012d7b38330d3392d4c232f13bb7
Valeria Bushueva <valeryabushuyeva@gmail.com>
2021-07-14 19:40:03 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {getImages, searchImages} from './api';
+import React, {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [imageList, setImageList] = useState([]);
+    const [nextCursor, setNextCursor] = useState(null);
+    const [searchValue, setSearchValue] = useState('');
+    useEffect(() => {
+        const fetchData = async () => {
+            const responseJson = await getImages();
+            setImageList(responseJson.resources);
+            setNextCursor(responseJson.next_cursor);
+        }
+
+        fetchData();
+    }, []);
+    const handleLoadMoreButtonClick = async () => {
+        const respponseJson = await getImages(nextCursor);
+        setImageList((currentImageList) => [...currentImageList, ...respponseJson.resources]);
+        setNextCursor(respponseJson.next_cursor);
+    };
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        const responseJson = await searchImages(searchValue, nextCursor);
+        setImageList(responseJson.resources);
+        setNextCursor(responseJson.next_cursor);
+    };
+    const resetForm = async () => {
+        const responseJson = await getImages(nextCursor);
+        setImageList(responseJson.resources);
+        setNextCursor(nextCursor);
+        setSearchValue('');
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={handleFormSubmit} className="search-bar">
+                <input value={searchValue}
+                       onChange={(event) => setSearchValue(event.target.value)}
+                       required='required' placeholder="Enter a search value..."></input>
+                <button type="submit" className="search-button">Search</button>
+                <button type="button" onClick={resetForm} className="seach-button">Clear</button>
+            </form>
+            <div className="image-grid">
+                {imageList.map((image) => <img src={image.url} alt={image.public_id}></img>)}
+            </div>
+            <div className="footer">
+                {nextCursor && <button onClick={handleLoadMoreButtonClick}>Load More</button>}
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
---
+++ src/api.js
@@ -0,0 +1,21 @@
+const API_URL = "http://localhost:7000"
+export const getImages = async (nextCursor) => {
+    const params = new URLSearchParams();
+    if (nextCursor) {
+        params.append('next_cursor', nextCursor);
+    }
+    const response = await fetch(API_URL + `/photos?${params}`);
+    const responseJson = await response.json();
+
+    return responseJson;
+};
+export const searchImages = async (searchValue, nextCursor) => {
+    const params = new URLSearchParams();
+    params.append('expression', searchValue);
+    if (nextCursor) {
+        params.append('next-cursor', nextCursor);
+    }
+    const response = await fetch(API_URL + `/search?${params}`);
+    const responseJson = await response.json()
+    return responseJson
+}
